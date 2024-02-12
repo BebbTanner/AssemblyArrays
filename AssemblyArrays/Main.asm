37167963fe663e82804aa31075258fb4d7f99cd7
Tanner Bebb <17tbebb@gmail.com>
2024-02-12 08:11:29 -0800

    Fixed the declaration of myArray Fixed my pathing so that the prgram would run Added line 18, This should store the entire array in the esi register.

--- AssemblyArrays/Main.asm
+++ AssemblyArrays/Main.asm
@@ -1,18 +1,21 @@
 ;Tanner Bebb
 ;Assembly language arrays
 
+includelib irvine32.lib
 .386
 .model flat, stdcall
 .stack 4096
-ExitProcess proto, dwExitCode:dwords
+ExitProcess proto, dwExitCode:dword
 
 .data
 
-myArray DW 1, 2, 3, 4, 5			;Created myArray
+myArray WORD 1, 2, 3, 4, 5			;Created myArray
 
 .code
 
 main proc
+
+mov esi, OFFSET myArray + 4			;What does this do?
 
 invoke ExitProcess, 0
 main endp
