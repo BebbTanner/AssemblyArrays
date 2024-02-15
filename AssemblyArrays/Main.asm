55e3155226323c0a554eed79d8e426e4b9aea9e7
Tanner Bebb <17tbebb@gmail.com>
2024-02-15 09:19:54 -0800

    Added lines 18 - 22 with documentation. These lines are just the setup for the for-loop.

--- AssemblyArrays/Main.asm
+++ AssemblyArrays/Main.asm
@@ -14,6 +14,14 @@ main PROC
 again:
 	cmp myNum, 10		;This will compare the most recent value in myNum to the value 10
 	jg done				;If the value in myNum is greater than 10, It will stop the loop.
+
+	mov eax, myNum		;Moves the current value of myNum into the eax register.
+	call WriteInt		;This will write the integer to the screen.
+	mov al, ' '			;Moves a space into the al register.
+	call WriteChar		;This writes the char stored in the al register.
+	inc myNum			;Increments the value of myNum.
+
+	jmp again			;If the value of myNum is not 10, This will go back and run the loop again.
 done:
 
 	call Crlf
